81de826305d95e9abedcf18e8717e10ae6e3ba01
TeofilBG <122180157+TeofilBG@users.noreply.github.com>
2026-06-07 04:12:58 +0200

    Fix encoder edit CC bank lookup

---
+++ graphics.h
@@ -0,0 +1,57 @@
+#ifndef GRAPHICS_H
+#define GRAPHICS_H
+
+#include <Arduino.h>
+#include <U8g2lib.h>
+
+namespace Graphics {
+  const uint8_t TITLE_X = 24;
+  const uint8_t TITLE_Y = 7;
+  const uint8_t CHANNEL_X = 38;
+  const uint8_t CHANNEL_Y = 23;
+  const uint8_t ENC1_X = 1;
+  const uint8_t ENC2_X = 86;
+  const uint8_t ENCODER_Y = 31;
+  const uint8_t EDIT_LABEL_X = 1;
+  const uint8_t EDIT_LABEL_Y = 18;
+  const uint8_t EDIT_NOTE_X = 82;
+  const uint8_t EDIT_NOTE_Y = 23;
+  const uint8_t EDIT_STATUS_X = 1;
+  const uint8_t EDIT_STATUS_Y = 31;
+  const uint8_t EDIT_TYPE_X = 72;
+  const uint8_t EDIT_TYPE_Y = 31;
+
+  inline void drawStatusScreen(U8G2 &oled, const char *channelText, const char *enc1Text, const char *enc2Text) {
+    oled.clearBuffer();
+
+    oled.setFont(u8g2_font_5x7_tf);
+    oled.drawStr(TITLE_X, TITLE_Y, "EUCALIPTUS MIDI");
+
+    oled.setFont(u8g2_font_10x20_tf);
+    oled.drawStr(CHANNEL_X, CHANNEL_Y, channelText);
+
+    oled.setFont(u8g2_font_5x7_tf);
+    oled.drawStr(ENC1_X, ENCODER_Y, enc1Text);
+    oled.drawStr(ENC2_X, ENCODER_Y, enc2Text);
+
+    oled.sendBuffer();
+  }
+
+  inline void drawMidiNoteEditScreen(U8G2 &oled, const char *noteText, const char *typeText, const char *statusText) {
+    oled.clearBuffer();
+
+    oled.setFont(u8g2_font_6x12_tf);
+    oled.drawStr(EDIT_LABEL_X, EDIT_LABEL_Y, "MIDI val");
+
+    oled.setFont(u8g2_font_10x20_tf);
+    oled.drawStr(EDIT_NOTE_X, EDIT_NOTE_Y, noteText);
+
+    oled.setFont(u8g2_font_5x7_tf);
+    oled.drawStr(EDIT_STATUS_X, EDIT_STATUS_Y, statusText);
+    oled.drawStr(EDIT_TYPE_X, EDIT_TYPE_Y, typeText);
+
+    oled.sendBuffer();
+  }
+}
+
+#endif
